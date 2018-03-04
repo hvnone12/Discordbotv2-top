928d53e7b5f8afca0236ea6b6e85762d174ea358
hvnone12 <imbrea97@gmail.com>
2018-03-05 00:22:00 +0200

    2

--- selfbot/self_config.py
+++ selfbot/self_config.py
@@ -3,5 +3,5 @@ target_servers=[419915259797569546] #Int list
 target_channels=[419919229492396034] #Int list
 destination_channels=[419915402605232141] #Int list
 # OBS! The server and channel ID, and destination channel ID have to be in the same list index value [0], [0] and [0] will be for the same result.
-postgres_uri='postgres://owjqwxorzljcdy:9ddf2cede1fa6705c6ca76709a23d0eb37b804a9c6b99e1c472cdc4da38bc513@ec2-79-125-110-209.eu-west-1.compute.amazonaws.com:5432/de0hfofi3l208r'
+postgres_uri='postgres://egzbneobhnwxnc:6527e00fc0444fe136bf7603e7d2a5de68d65db5211c7d9d2dcc34dd91db7961@ec2-54-247-81-88.eu-west-1.compute.amazonaws.com:5432/d389t0hq8oqjse'
 self_token="Mjc1NDM3NDk0NzUxNzIzNTIx.DPpDhQ.PYM9zlqpXnvcnFWspH4feXBYM9g"
